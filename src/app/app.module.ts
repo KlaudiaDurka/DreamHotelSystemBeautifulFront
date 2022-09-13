eba7ba187f58099a1784dc6cf51d1da6cbe179cf
Tomek <guziktomasz@o2.pl>
2022-09-13 21:10:10 +0200

    working get room from spring endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomComponent } from './room/room.component';
+import {RoomServiceService} from "./room/room-service.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { RoomComponent } from './room/room.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [RoomServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
